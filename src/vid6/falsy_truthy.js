1e81e7022b6b6f5c49beb20a090dea3774384674
Anas Mamlouk <anas.mamlouk@yahoo.fr>
2024-08-24 11:50:26 +0200

    vid 6 truthy falsy

---
+++ src/vid6/falsy_truthy.js
@@ -0,0 +1,9 @@
+let value = "false";
+let value2 = 3 * "true";
+console.log(value2.typeof);
+console.log(value2);
+if (value2) {
+  console.log("Truthy value");
+} else {
+  console.log("Falsy value");
+}
